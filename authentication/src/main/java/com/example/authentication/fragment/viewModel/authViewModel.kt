0a51516e2701b8445610e5e6751465faf3b8c476
ILLIA <jointlord0@gmail.com>
2022-08-01 23:59:04 +0300

    added login in VM

--- authentication/src/main/java/com/example/authentication/fragment/viewModel/authViewModel.kt
+++ authentication/src/main/java/com/example/authentication/fragment/viewModel/authViewModel.kt
@@ -3,7 +3,9 @@ package com.example.authentication.fragment.viewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import com.example.core.model.result
+import com.example.core.model.ErrorEntity
+import com.example.core.model.Result
+import com.example.core.model.product
 import com.example.core.model.user
 import com.example.core.useCases.loginUser
 import com.example.core.useCases.registrateUser
@@ -14,12 +16,37 @@ class authViewModel @Inject constructor(
     private val registrateUser: registrateUser,
 ) : ViewModel() {
 
-    private val loginState = MutableLiveData<result<user>> ()
-    val _loginState: LiveData<result<user>>
+    private val loginState = MutableLiveData<Result<user>> ()
+    val _loginState: LiveData<Result<user>>
         get() = loginState
 
-    fun login(user: user) {
-        loginState.postValue(loginUser.login(user))
+
+    fun login(login: String, password: String){
+        loginState.postValue(
+            loginUser.login(user("","",login,"","","",null,null,password))
+        )
+    }
+
+    fun registrate(name: String,
+              surname: String,
+              login: String,
+              photo: String?,
+              telephone: String,
+              lotsList: List<product>?,
+              purchaseList: List<product>?,
+              password: String) {
+
+        val loginRegex = Regex(pattern = "^(?=[a-zA-Z0-9._]{8,20}\$)(?!.*[_.]{2})[^_.].*[^_.]\$")
+        if(!loginRegex.matches(login)){
+            loginState.postValue(Result.Error(ErrorEntity.WrongCredentialsLogin))
+            return
+        }
+        val emailRegex = Regex(pattern = "^[A-Za-z](.*)([@]{1})(.{1,})(\\\\.)(.{1,})")
+        if(!loginRegex.matches(login)){
+            loginState.postValue(Result.Error(ErrorEntity.WrongCredentialsLogin))
+            return
+        }
+  //      loginState.postValue(loginUser.login(user))
 
     }
 }
--- core/src/main/java/com/example/core/model/ErrorEntity.kt
+++ core/src/main/java/com/example/core/model/ErrorEntity.kt
@@ -3,7 +3,13 @@ package com.example.core.model
 sealed class ErrorEntity {
     object Network : ErrorEntity()
 
-    object WrongCredentials : ErrorEntity()
+    object WrongCredentialsLogin : ErrorEntity()
+
+    object WrongCredentialsEmail : ErrorEntity()
+
+    object WrongCredentialsPassword : ErrorEntity()
+
+    object WrongLoginOrPass : ErrorEntity()
 
     object MissToken : ErrorEntity()
 
